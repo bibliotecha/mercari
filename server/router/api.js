6f4bc2c99156964d06bb62b697f563c6285f3d1a
Ken <a01056715@gmail.com>
2020-06-25 14:25:06 -0700

    バグ修正

--- server/router/api.js
+++ server/router/api.js
@@ -1,12 +1,11 @@
 const pool = require('../database/db');
 const express = require('express');
-const { seedItems } = require('../database/seed');
-const { query } = require('express');
+// const { seedItems } = require('../database/seed');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   pool.getConnection((err, connection) => {
-    query = connection.query('select * from items', (err, result) => {
+    connection.query('select * from items', (err, result) => {
       if (err) throw err;
       res.status(200).json(result);
     });
