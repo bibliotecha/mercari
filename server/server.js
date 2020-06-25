ed83cc8892589302d3a297228745b494f1d055d2
Ken <a01056715@gmail.com>
2020-06-25 00:42:12 -0700

    dotenvを追加しました

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
-/**
- * Expressのツールをファイル内で使えるようにします。
- */
+require('dotenv').config();
 const express = require('express');
-/**
- * 'path'というモジュールをファイル内で使えるようにします
- */
-const path = require('path');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: 'mercari',
+  post: 3307,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connectin: ${err.stack}`);
+  }
+
+  console.log(`connected as id ${connection.threadId}`);
+});
 
 /**
  * サーバーのインスタンスを定義
@@ -16,16 +26,6 @@ const app = express();
  * 使えるポートを確保
  */
 const PORT = 3000;
-
-// styles.cssを取り込む
-app.use(express.static(path.join(__dirname, '../client')));
-
-// 'localhost:3000 + '/'と入力する中の関数を呼ぶ
-app.get('/', (req, res) => {
-  // 一つ親の階層の'client'というフォルダに入る
-  //　index.htmlというファイルがあるのでそれをブラウザに送ってください
-  res.sendFile(path.join(__dirname, '../client', 'landing.html'));
-});
 
 //　3000というポートにリクエストが来たら対応してください
 app.listen(PORT, () => {
