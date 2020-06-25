ff081c3ea270e3d1977924ca84db8eadeb2ee26d
Ken <a01056715@gmail.com>
2020-06-25 03:57:43 -0700

    Corsを直しました

--- server/server.js
+++ server/server.js
@@ -10,6 +10,15 @@ const cors = require('cors');
  */
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mercariapp.netlify.app/');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(cors());
 
 app.use('/api', apiRouter);
