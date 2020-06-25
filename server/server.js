9722713056c7f0ba2fd2e20b7ace06cff0feae8d
Ken <a01056715@gmail.com>
2020-06-25 02:09:34 -0700

    サーバー側がクライアント側の要求に対してAPIを提供するように設定しました

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('./database/db');
 const express = require('express');
 const apiRouter = require('./router/api');
+const cors = require('cors');
 
 /**
  * サーバーのインスタンスを定義
@@ -12,6 +13,8 @@ const app = express();
  * 使えるポートを確保
  */
 const PORT = 4000;
+
+app.use(cors());
 
 app.use('/api', apiRouter);
 
