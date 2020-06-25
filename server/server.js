5d096e6efa7a3bf3424430203745fd32dee424c0
Ken <a01056715@gmail.com>
2020-06-25 04:33:04 -0700

    サーバーリクエストヘッダーの修正

--- server/server.js
+++ server/server.js
@@ -14,21 +14,10 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api', apiRouter);
+app.use(cors());
+app.options('*', cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  ); // If needed
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  ); // If needed
-  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-  next();
-});
+app.use('/api', apiRouter);
 
 //　3000というポートにリクエストが来たら対応してください
 app.listen(config.port, () => {
