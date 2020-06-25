f3bd634bfa03e9d92f86c71e9871638eb02183d9
Ken <a01056715@gmail.com>
2020-06-25 14:23:07 -0700

    バグ修正

--- server/database/db.js
+++ server/database/db.js
@@ -2,19 +2,11 @@ const mysql = require('mysql');
 
 const config = require('../config');
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: config.host,
   user: config.user,
   password: config.password,
   database: config.db,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error(`error connectin: ${err.stack}`);
-  }
-
-  console.log(`connected as id ${db.threadId}`);
-});
-
-module.exports = db;
+module.exports = pool;
--- server/router/api.js
+++ server/router/api.js
@@ -1,48 +1,44 @@
-const db = require('../database/db');
+const pool = require('../database/db');
 const express = require('express');
 const { seedItems } = require('../database/seed');
+const { query } = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  db.query('select * from items', (err, result) => {
-    if (err) {
-      return res.status(401).send('まだデータが存在していません');
-    }
-    return res.status(200).json(result);
+  pool.getConnection((err, connection) => {
+    query = connection.query('select * from items', (err, result) => {
+      if (err) throw err;
+      res.status(200).json(result);
+    });
+    connection.release();
   });
 });
 
-router.get('/droptable', (req, res) => {
-  let dropTable = 'DROP TABLE items';
-  db.query(dropTable, (err, result) => {
-    if (err) {
-      return res.status(401).send('無くすテーブルが存在していません');
-    }
-    console.log('dropped table');
-  });
-  return res.status(200).send('tableを無事に消しました');
-});
+// router.get('/droptable', (req, res) => {
+//   let dropTable = 'DROP TABLE items';
+//   db.query(dropTable, (err, result) => {
+//     if (err) {
+//       return res.status(401).send('無くすテーブルが存在していません');
+//     }
+//     console.log('dropped table');
+//   });
+//   return res.status(200).send('tableを無事に消しました');
+// });
 
-router.get('/createtable', (req, res) => {
-  let createTables =
-    'CREATE TABLE items (id SERIAL PRIMARY KEY, name VARCHAR(255), url VARCHAR(255), description VARCHAR(255))';
-  db.query(createTables, (err, result) => {
-    if (err) {
-      return res.status(401).send('すでにテーブルが存在しています');
-    }
-    console.log('created table');
-  });
-  return res.status(200).send('tableを無事に作りました');
-});
+// router.get('/createtable', (req, res) => {
+//   let createTables =
+//     'CREATE TABLE items (id SERIAL PRIMARY KEY, name VARCHAR(255), url VARCHAR(255), description VARCHAR(255))';
+//   db.query(createTables, (err, result) => {
+//     if (err) {
+//       return res.status(401).send('すでにテーブルが存在しています');
+//     }
+//   });
+//   return res.status(200).send('tableを無事に作りました');
+// });
 
-router.get('/seed', (req, res) => {
-  seedItems();
-  return res.status(200).send('seedを完了しました');
-});
+// router.get('/seed', (req, res) => {
+//   seedItems();
+//   return res.status(200).send('seedを完了しました');
+// });
 
 module.exports = router;
