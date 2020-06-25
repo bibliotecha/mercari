5325fb5b8a135b9dfd26fe6d24d2168e5bb88887
Ken <a01056715@gmail.com>
2020-06-25 03:47:09 -0700

    エラーが出力される際にブラウザに表示するようにしました

--- server/router/api.js
+++ server/router/api.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   db.query('select * from items', (err, result) => {
-    if (err) throw err;
+    if (err) {
+      res.status(401).json('まだデータが存在していません');
+    }
     res.status(200).json(result);
   });
 });
@@ -13,7 +15,9 @@ router.get('/', (req, res) => {
 router.get('/droptable', (req, res) => {
   let dropTable = 'DROP TABLE items';
   db.query(dropTable, (err, result) => {
-    if (err) throw err;
+    if (err) {
+      res.status(401).json('無くすテーブルが存在していません');
+    }
     console.log('dropped table');
   });
   res.status(200).send('tableを無事に消しました');
@@ -23,7 +27,9 @@ router.get('/createtable', (req, res) => {
   let createTables =
     'CREATE TABLE items (id SERIAL PRIMARY KEY, name VARCHAR(255), url VARCHAR(255), description VARCHAR(255))';
   db.query(createTables, (err, result) => {
-    if (err) throw err;
+    if (err) {
+      res.status(401).json('すでにテーブルが存在しています');
+    }
     console.log('created table');
   });
   res.status(200).send('tableを無事に作りました');
