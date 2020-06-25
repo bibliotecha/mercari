d25dcb9592a2437bded94a992f0ff9d7dde3c231
Ken <a01056715@gmail.com>
2020-06-25 14:27:48 -0700

    バグ修正

--- server/router/api.js
+++ server/router/api.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   pool.getConnection((err, connection) => {
+    if (err) throw err;
     connection.query('select * from items', (err, result) => {
       if (err) throw err;
       res.status(200).json(result);
@@ -12,32 +13,5 @@ router.get('/', (req, res) => {
     connection.release();
   });
 });
-
-// router.get('/droptable', (req, res) => {
-//   let dropTable = 'DROP TABLE items';
-//   db.query(dropTable, (err, result) => {
-//     if (err) {
-//       return res.status(401).send('無くすテーブルが存在していません');
-//     }
-//     console.log('dropped table');
-//   });
-//   return res.status(200).send('tableを無事に消しました');
-// });
-
-// router.get('/createtable', (req, res) => {
-//   let createTables =
-//     'CREATE TABLE items (id SERIAL PRIMARY KEY, name VARCHAR(255), url VARCHAR(255), description VARCHAR(255))';
-//   db.query(createTables, (err, result) => {
-//     if (err) {
-//       return res.status(401).send('すでにテーブルが存在しています');
-//     }
-//   });
-//   return res.status(200).send('tableを無事に作りました');
-// });
-
-// router.get('/seed', (req, res) => {
-//   seedItems();
-//   return res.status(200).send('seedを完了しました');
-// });
 
 module.exports = router;
