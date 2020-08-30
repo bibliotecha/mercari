5656231f608d5ab259a888b3b77f73cd6980cc0b
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-30 12:03:11 -0700

    fixed ad icon sizes & img file names

--- client/src/components/landing/AppAdvertisement/AppAdvertisement.js
+++ client/src/components/landing/AppAdvertisement/AppAdvertisement.js
@@ -1,23 +1,42 @@
 import React from 'react';
-
-// Css
 import './AppAdvertisement.styles.css';
-
-// イメージ
-import AppIcon from '../../../assets/img/mercari_app_icon.png';
+import AppIcon from '../../../assets/img/app_icon.png';
+import { AppStore } from '../../../assets/svg';
+import GooglePlay from '../../../assets/img/google-play-badge.png';
+import AD_PHONES from '../../../assets/img/ad_phones.png';
 
 export const AppAdvertisement = () => {
   return (
-    <section className='app__advertisement'>
-      <div className='app__advertisement--icon'>
-        <img src={AppIcon} alt='app_icon' />
-      </div>
-      <div className='app__advertisement--text'>
-        <div>
-          <p>スマホでかんたんフリマアプリ</p>
+    <section class='app-section'>
+      <div class='app-section__content'>
+        <div class='app-section__icons-app-mobile'>
+          <img src={AppIcon} alt='app-icon' width='50px' />
         </div>
+        <div class='app-section__content-titles'>
+          <div class='app-section__subtitle'>
+            <h3>スマホでかんたんフリマアプリ</h3>
+          </div>
+          <div class='app-section__title'>
+            <h2>いますぐ無料ダウンロード！</h2>
+          </div>
+        </div>
+
+        <div class='app-section__icons'>
+          <div class='app-section__icons-app'>
+            <img src={AppIcon} alt='app-icon' />
+          </div>
+          <div class='app-section__icons--appstore'>
+            <AppStore />
+          </div>
+          <div class='app-section__icons-gpstore'>
+            <img src={GooglePlay} alt='google-play' />
+          </div>
+        </div>
+      </div>
+
+      <div class='app-section__phone-img'>
         <div>
-          <p>今すぐ無料ダウンロード！</p>
+          <img src={AD_PHONES} width='300px' alt='phone' />
         </div>
       </div>
     </section>
