13753b1a9a30a1d7451a97d72ce916a3b1f9292f
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-30 21:36:44 -0700

    test

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -12,7 +12,7 @@ export default () => {
     <>
       <Header />
       <BannerSection />
-      <Items />
+      {/* <Items /> */}
       <AppAdvertisement />
       <Footer />
       <FloatButton />
