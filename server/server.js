c943fd4e9c1ece689f2e13bd5f8da0fcedcaf9ae
Kenichi Okiebisu <a01056715@gmail.com>
2020-09-13 16:45:01 -0700

    itemsのpost methodを追加

--- server/server.js
+++ server/server.js
@@ -1,28 +1,44 @@
-require('dotenv').config();
-require('./database/db');
-const config = require('./config');
 const express = require('express');
-const apiRouter = require('./router/api');
-const cors = require('cors');
-
-/**
- * サーバーのインスタンスを定義
- */
+const fs = require('fs');
 const app = express();
+const port = 4000;
 
 app.use(express.json());
 
-app.use(cors());
-app.options('*', cors());
+const data = JSON.parse(
+  fs.readFileSync('./data.json', (data) => {
+    data;
+  })
+);
 
-app.get('/', (req, res) => {
-  return res.status(200).send('hello world!');
+app.get('/items', (req, res) => {
+  return res.status(200).json({
+    status: 'successful',
+    data,
+  });
 });
 
-app.use('/api', apiRouter);
+app.get('/items/:id', (req, res) => {
+  console.log(req.params);
+});
 
-//　3000というポートにリクエストが来たら対応してください
-app.listen(config.port, () => {
-  // ``で囲むことにより変数を入れられる
-  console.log(`server is listening on port ${config.port}`);
+app.post('/items', (req, res) => {
+  console.log(req.body);
+  const newId = data.items[data.items.length - 1].id + 1;
+  const newItem = Object.assign({}, { id: newId }, req.body);
+  // ファイル内の配列に追加
+  data.items.push(newItem);
+  fs.writeFileSync('./data.json', JSON.stringify(data), (err, data) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        items: newItem,
+      },
+    });
+  });
+  //res.send('Done'); // レスポンスが２度送られるためエラーが出る
+});
+
+app.listen(port, () => {
+  console.log('サーバーが立ち上がりました');
 });
