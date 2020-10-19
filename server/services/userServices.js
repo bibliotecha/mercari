ceaadaac34c3e09d14b65394d98b49d6fd505182
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-19 16:57:54 -0700

    wrote login of signup

--- server/services/userServices.js
+++ server/services/userServices.js
@@ -1,23 +1,29 @@
-const { db } = require('../db');
+const db = require('../db');
 
-exports.signup = (req, res) => {
-  console.log('signup body', req.body);
+exports.signup = async (req, res) => {
   // if there is email
   // error response
   try {
-    const result = await db.query('SELECT * FROM user WHERE email=$1 AND password=$2', [req.body.email. req.body.password])
-    console.log(result)
-    // attach cookie
+    console.log('entered', req.body.email, req.body.password);
+    const result = await db.query(
+      'SELECT * FROM "user" WHERE email=$1 AND password=$2',
+      [req.body.email, req.body.password]
+    );
+    if (result.rows.length === 0) {
+      res.status(400).json({
+        status: 'fail',
+        message: 'email or password is wrong',
+      });
+    }
     res.status(200).json({
-      status: ''
+      status: 'success',
     });
   } catch (err) {
     res.status(400).json({
       status: 'error',
-      message: 'error happened on the server'
+      message: 'error during the process',
     });
   }
- 
 };
 
 exports.login = (req, res) => {
@@ -30,34 +36,34 @@ exports.login = (req, res) => {
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'route is not yet defined',
+    message: 'route is not yet defined for getallusers',
   });
 };
 
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'route is not yet defined',
+    message: 'route is not yet defined for createuser',
   });
 };
 
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'route is not yet defined',
+    message: 'route is not yet defined for getuser',
   });
 };
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'route is not yet defined',
+    message: 'route is not yet defined for updateuser',
   });
 };
 
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'route is not yet defined',
+    message: 'route is not yet defined for deleteuser',
   });
 };
