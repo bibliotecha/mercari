ecc360541dde49d4f8058ae0ecf061ce4d11c169
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-19 16:39:24 -0700

    added user table to seed.sql

--- server/db/seed.sql
+++ server/db/seed.sql
@@ -1,22 +1,42 @@
 CREATE TABLE item (
     id BIGSERIAL PRIMARY KEY NOT NULL,
-    imgUrl VARCHAR(100) NOT NULL,
+    "imgUrl" VARCHAR(100) NOT NULL,
     price INT NOT NULL,
     description VARCHAR(100),
     likes INT DEFAULT 0
 );
 
 
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
-INSERT INTO item(imgUrl, price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+INSERT INTO item("imgUrl", price, description) VALUES ('https://static.mercdn.net/thumb/photos/m61047450015_1.jpg?1602292425', 4500, '☆リーバイス☆LEVI''S550 ブーツカットデニムパンツ　ジーンズ');
+
+-- ここではuserはreserved wordなので""で囲ってあげる
+CREATE TABLE "user" (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    nickname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    "firstName" VARCHAR(100) NOT NULL,
+    "lastName" VARCHAR(100) NOT NULL,
+    "firstNameKana" VARCHAR(100) NOT NULL,
+    "lastNameKana" VARCHAR(100) NOT NULL,
+    year INT NOT NULL,
+    month INT NOT NULL,
+    day INT NOT NULL
+);
+
+INSERT INTO "user" (nickname, email, password, "firstName", "lastName", "firstNameKana", "lastNameKana", year, month, day) VALUES ('やまさん', 'yamato@gmail.com', 'yamato1234', '大和', '撫子', 'ヤマト', 'ナデシコ', 2012, 12, 31);
+INSERT INTO "user" (nickname, email, password, "firstName", "lastName", "firstNameKana", "lastNameKana", year, month, day) VALUES ('えり', 'erika@gmail.com', 'eri1234', '大和', 'えり', 'ヤマト', 'エリ', 2012, 12, 31);
+INSERT INTO "user" (nickname, email, password, "firstName", "lastName", "firstNameKana", "lastNameKana", year, month, day) VALUES ('たけ', 'takeru@gmail.com', 'take1234', '大和', 'たけ', 'ヤマト', 'タケ', 2012, 12, 31);
+INSERT INTO "user" (nickname, email, password, "firstName", "lastName", "firstNameKana", "lastNameKana", year, month, day) VALUES ('まさ', 'masa@gmail.com', 'masa1234', '大和', 'まさる', 'ヤマト', 'マサル', 2012, 12, 31);
